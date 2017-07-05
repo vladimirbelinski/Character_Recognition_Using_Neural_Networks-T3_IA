e3b6b4fb58e14e7b106c8abdd818a6789b3001cb
Matheus DallRosa <matheusdallrosa94@gmail.com>
2017-07-05 11:20:45 -0300

    Neurons are now in a grid, fixed sq distance for matrices, rand between [0..RAND_MAX]

--- main.cpp
+++ main.cpp
@@ -3,43 +3,47 @@
 using namespace std;
 
 const int SIZE = 32;
-const int NEURONS = 64;
+const int NEURONS = 8;
 const int TEST_SIZE = 134;
 const int TRAIN_SIZE = 1800;
 double alpha = .1;
 const double sigma = 16;
 
-struct Matrix{  
+#define f first
+#define s second
+
+typedef pair<int,int> ii;
+
+struct Matrix{ 
   double cells[SIZE][SIZE];
-  Matrix();
+  Matrix(){}
   Matrix(double _cells[SIZE][SIZE]){
     memcpy(cells,_cells,sizeof cells);
-  }
-  
+  }  
   Matrix operator-(Matrix &o){
     double r[SIZE][SIZE];
     for(int i = 0; i < SIZE; i++)
       for(int j = 0; j < SIZE; j++)
-        r[i][j] = cells[i][j]-o.cells[i][j];
+        r[i][j] = cells[i][j]-o[i][j];
     return Matrix(r);
-  }
-  
+  }  
   Matrix operator+(Matrix &o){
     double r[SIZE][SIZE];
     for(int i = 0; i < SIZE; i++)
       for(int j = 0; j < SIZE; j++)
-        r[i][j] = cells[i][j]+o.cells[i][j];
+        r[i][j] = cells[i][j]+o[i][j];
     return Matrix(r);
-  }
-  
+  }  
   Matrix operator*(double scale){
     double r[SIZE][SIZE];
     for(int i = 0; i < SIZE; i++)
       for(int j = 0; j < SIZE; j++)
         r[i][j] = cells[i][j]*scale;
     return Matrix(r);
-  }
-  
+  }  
+  double* operator[](int i){
+    return cells[i];
+  }  
   void print(){
     for(int i = 0; i < SIZE; i++){
       for(int j = 0; j < SIZE; j++)
@@ -53,14 +57,21 @@ double sq_euclidean_distance(Matrix &a,Matrix &b){
   double sq_distance = 0.;
   for(int i = 0; i < SIZE; i++){
     for(int j = 0; j < SIZE; j++){
-      double dij = a.cells[i][j] - b.cells[i][j];
+      double dij = a[i][j] - b[i][j];
       sq_distance += dij*dij;
     }
   }
   return sq_distance;
 }
 
-vector<Matrix> train,test,neurons;
+double sq_euclidean_distance(ii a,ii b){
+  double dx = a.f-b.f;
+  double dy = a.s-b.s;
+  return dx*dx+dy*dy;
+}
+
+vector<Matrix> train,test;
+Matrix neurons[NEURONS][NEURONS];
 void read_digits(vector<Matrix> &m,int a,FILE *src){
   for (int i = 0,c; i < a; i++) {    
     double digit[SIZE][SIZE];
@@ -76,34 +87,38 @@ void read_digits(vector<Matrix> &m,int a,FILE *src){
 }
 
 void init_neurons(){
-  const int interval = 2;  
   srand(time(NULL));
-  for(int i = 0; i < NEURONS; i++){
-    double matrix[SIZE][SIZE];    
-    for(int j = 0; j < SIZE; j++)
-      for(int k = 0; k < SIZE; k++)
-        matrix[j][k] = rand() % interval;    
-    neurons.push_back(Matrix(matrix));
-  }
+  for(int i = 0; i < NEURONS; i++)
+    for(int i2 = 0; i2 < NEURONS; i2++)
+      for(int j = 0; j < SIZE; j++)
+        for(int k = 0; k < SIZE; k++)
+          neurons[i][i2][j][k] = (double)rand() / RAND_MAX;                         
 }
 
-void train_neurons(){
-  for(int i = 0; i < TRAIN_SIZE; i++){        
-    //searching for the closest neuron.    
-    Matrix BMU = neurons[0];
-    double min_dist = sq_euclidean_distance(train[i],BMU);
-    for(int j = 1; j < NEURONS; j++){      
-      double dist_ji = sq_euclidean_distance(train[i],neurons[j]);
+ii closest_neuron(Matrix &m){
+  ii BMU;
+  double min_dist = SIZE*SIZE;
+  for(int j = 0; j < NEURONS; j++)    
+    for(int j2 = 0; j2 < NEURONS; j2++){      
+      double dist_ji = sq_euclidean_distance(m,neurons[j][j2]);
       if(dist_ji < min_dist){        
-        BMU = neurons[j];
+        BMU = ii(j,j2);
         min_dist = dist_ji;
       }
-    }  
-    //updating the neighboring of BMU.         
+    }
+  return BMU;
+}
+
+void train_neurons(){  
+  for(int i = 0; i < TRAIN_SIZE; i++){                
+    ii BMU = closest_neuron(train[i]);    
+    //updating the neighborhood of BMU.         
     for(int j = 0; j < NEURONS; j++){       
-      double dist = sq_euclidean_distance(BMU,neurons[j]);                     
-      Matrix shift = (train[i]-neurons[j])*pow(M_E, -dist/sigma)*alpha;
-      neurons[j] = neurons[j] + shift;          
+      for(int j2 = 0; j2 < NEURONS; j2++){        
+        double dist = sq_euclidean_distance(BMU,ii(j,j2));                     
+        Matrix shift = (train[i]-neurons[j][j2])*pow(M_E, -dist/sigma)*alpha;
+        neurons[j][j2] = neurons[j][j2] + shift;  
+      }        
     }
   }
 }
@@ -121,26 +136,27 @@ int main(int argc, char const *argv[]) {
   read_digits(test,TEST_SIZE,testing_file);  
   fclose(testing_file);  
   
-  init_neurons();     
-  for(int l = 0; alpha > 0; l++ ){
+  init_neurons();       
+  for(int l = 0; alpha > 0; l++,alpha -= .001 ){
     train_neurons();  
     for(int i = 0; i < NEURONS; i++){
-      int mn = 0;
-      double mn_dist = sq_euclidean_distance(neurons[i],train[mn]);
-      for(int j = 1; j < TRAIN_SIZE; j++){
-        double dist = sq_euclidean_distance(neurons[i],train[j]);
-        if(mn_dist > dist){
-          mn = j;
-          mn_dist = dist;
+      for(int i2 = 0; i2 < NEURONS; i2++){
+        int mn = 0;
+        double mn_dist = sq_euclidean_distance(neurons[i][i2],train[mn]);
+        for(int j = 1; j < TRAIN_SIZE; j++){
+          double dist = sq_euclidean_distance(neurons[i][i2],train[j]);
+          if(mn_dist > dist){
+            mn = j;
+            mn_dist = dist;
+          }
         }
+        printf("%d\n",i);
+        neurons[i][i2].print();
+        printf("\n");
+        train[mn].print();
+        printf("\n");      
       }
-      printf("%d\n",i);
-      neurons[i].print();
-      printf("\n");
-      train[mn].print();
-      printf("\n");      
-    }
-    alpha -= .001;
+    }    
   }
   return 0;
 }
