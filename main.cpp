fd5818f72e5c2a68a0247e0204416b3283b17051
Gabriel Galli <g7.galli96@gmail.com>
2017-07-07 09:52:15 -0300

    Implemented load_neurons

--- main.cpp
+++ main.cpp
@@ -14,41 +14,41 @@ double alpha = .1;
 
 typedef pair<int,int> ii;
 
-struct Matrix{   
+struct Matrix{
   int digit;
   double cells[SIZE][SIZE];
-  Matrix(){}  
+  Matrix(){}
   Matrix(double _cells[SIZE][SIZE]){
     memcpy(cells,_cells,sizeof cells);
-  }  
+  }
   Matrix(double _cells[SIZE][SIZE],int _digit){
     digit = _digit;
     memcpy(cells,_cells,sizeof cells);
-  }  
+  }
   Matrix operator-(Matrix &o){
     double r[SIZE][SIZE];
     for(int i = 0; i < SIZE; i++)
       for(int j = 0; j < SIZE; j++)
         r[i][j] = cells[i][j]-o[i][j];
     return Matrix(r);
-  }  
+  }
   Matrix operator+(Matrix &o){
     double r[SIZE][SIZE];
     for(int i = 0; i < SIZE; i++)
       for(int j = 0; j < SIZE; j++)
         r[i][j] = cells[i][j]+o[i][j];
     return Matrix(r);
-  }  
+  }
   Matrix operator*(double scale){
     double r[SIZE][SIZE];
     for(int i = 0; i < SIZE; i++)
       for(int j = 0; j < SIZE; j++)
         r[i][j] = cells[i][j]*scale;
     return Matrix(r);
-  }  
+  }
   double* operator[](int i){
     return cells[i];
-  }  
+  }
   void print(bool trunc){
     for(int i = 0; i < SIZE; i++){
       for(int j = 0; j < SIZE; j++)
@@ -81,15 +81,15 @@ Matrix neurons[NEURONS][NEURONS];
 void read_digits(vector<Matrix> &m,FILE *src){
   int a;
   fscanf(src,"%d\n",&a);
-  for (int i = 0,c; i < a; i++) {    
+  for (int i = 0,c; i < a; i++) {
     double digit[SIZE][SIZE];
     for(int j = 0; j < SIZE; j++){
-      for(int k = 0; k < SIZE; k++){        
+      for(int k = 0; k < SIZE; k++){
         fscanf(src," %c",&c);
-        digit[j][k] = c-'0';        
-      }      
+        digit[j][k] = c-'0';
+      }
     }
-    fscanf(src,"  %c ",&c);        
+    fscanf(src,"  %c ",&c);
     m.push_back(Matrix(digit,c-'0'));
   }
 }
@@ -100,16 +100,16 @@ void init_neurons(){
     for(int i2 = 0; i2 < NEURONS; i2++)
       for(int j = 0; j < SIZE; j++)
         for(int k = 0; k < SIZE; k++)
-          neurons[i][i2][j][k] = (double)rand() / RAND_MAX;                         
+          neurons[i][i2][j][k] = (double)rand() / RAND_MAX;
 }
 
 ii closest_neuron(Matrix &m){
   ii BMU;
   double min_dist = SIZE*SIZE;
-  for(int j = 0; j < NEURONS; j++)    
-    for(int j2 = 0; j2 < NEURONS; j2++){      
+  for(int j = 0; j < NEURONS; j++)
+    for(int j2 = 0; j2 < NEURONS; j2++){
       double dist_ji = sq_euclidean_distance(m,neurons[j][j2]);
-      if(dist_ji < min_dist){        
+      if(dist_ji < min_dist){
         BMU = ii(j,j2);
         min_dist = dist_ji;
       }
@@ -122,7 +122,7 @@ vector<int> randomic_order(int n){
   for(int i = 0; i < n; i++) elem.push_back(i);
   vector<int> order;
   while(!elem.empty()){
-    auto it = elem.begin();    
+    auto it = elem.begin();
     advance(it,rand()%elem.size());
     order.push_back(*it);
     elem.erase(it);
@@ -131,16 +131,16 @@ vector<int> randomic_order(int n){
 }
 
 void train_neurons(){
-  vector<int> training_order = randomic_order(train.size());  
-  for(int i = 0; i < (int)train.size(); i++){                
-    ii BMU = closest_neuron(train[training_order[i]]);    
-    //updating the neighborhood of BMU.         
-    for(int j = 0; j < NEURONS; j++){       
-      for(int j2 = 0; j2 < NEURONS; j2++){        
-        double dist = sq_euclidean_distance(BMU,ii(j,j2));                     
+  vector<int> training_order = randomic_order(train.size());
+  for(int i = 0; i < (int)train.size(); i++){
+    ii BMU = closest_neuron(train[training_order[i]]);
+    //updating the neighborhood of BMU.
+    for(int j = 0; j < NEURONS; j++){
+      for(int j2 = 0; j2 < NEURONS; j2++){
+        double dist = sq_euclidean_distance(BMU,ii(j,j2));
         Matrix shift = (train[i]-neurons[j][j2])*pow(M_E, -dist/sigma)*alpha;
-        neurons[j][j2] = neurons[j][j2] + shift;  
-      }        
+        neurons[j][j2] = neurons[j][j2] + shift;
+      }
     }
   }
 }
@@ -163,13 +163,13 @@ void print_matches(){
       printf("%d %d\n",i,i2);
       neurons[i][i2].print(true);
       printf("\n");
-      train[mn].print(true);            
+      train[mn].print(true);
       frequence[train[mn].digit]++;
       matches[i][i2] = train[mn].digit;
-      printf("\n");      
+      printf("\n");
     }
   }
-  for(int i = 0; i < 10; i++) printf("%d %d\n",i,frequence[i]);  
+  for(int i = 0; i < 10; i++) printf("%d %d\n",i,frequence[i]);
   for(int i = 0; i < NEURONS; i++){
     for(int i2 = 0; i2 < NEURONS; i2++)
       printf("%d ",matches[i][i2]);
@@ -178,6 +178,18 @@ void print_matches(){
 }
 
 void load_neurons(string dir){
+  FILE *neurons_file = fopen(dir.c_str(),"r");
+  for(int i = 0; i < NEURONS; i++)
+    for(int i2 = 0; i2 < NEURONS; i2++) {
+      double w;
+      double digit[SIZE][SIZE];
+      for(int j = 0; j < SIZE; j++)
+        for(int k = 0; k < SIZE; k++){
+          fscanf(neurons_file," %lf",&w);
+          digit[j][k] = w;
+        }
+      neurons[i][i2] = Matrix(digit);
+    }
 }
 
 void save_neurons(string dir){
@@ -187,29 +199,29 @@ void save_neurons(string dir){
 //--tra path/to/training/file
 //--lnet path/to/load/trained_neurons/file
 //--snet path/to/save/trained_neurons/file
-int main(int argc, char const *argv[]) {    
+int main(int argc, char const *argv[]) {
   map<string,string> param;
   for(int i = 1; i < argc; i += 2) param[argv[i]] = argv[i+1];
   if(param.find("--lnet") != param.end()) load_neurons(param["--lnet"]);
-  else init_neurons();    
-  
+  else init_neurons();
+
   if(param.find("--tra") != param.end()){
     //reading training set
     FILE *training_file = fopen(param["--tra"].c_str(),"r");
     read_digits(train,training_file);
     fclose(training_file);
     for(int l = 0; alpha > 0 && l < TRAIN_ITER; l++, alpha -= .0001)
-      train_neurons();          
+      train_neurons();
     print_matches();
   }
-  
+
   if(param.find("--tes") != param.end()){
     //reading testing set
     FILE *testing_file = fopen(param["--tes"].c_str(),"r");
-    read_digits(test,testing_file);  
-    fclose(testing_file);    
-  }     
-  
+    read_digits(test,testing_file);
+    fclose(testing_file);
+  }
+
   if(param.find("--snet") != param.end()) save_neurons(param["--snet"]);
   return 0;
 }
